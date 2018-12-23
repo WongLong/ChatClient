ae0c3ecc3717c3e5fa556d945acb90b0547db2d3
WongLong <35207741+WongLong@users.noreply.github.com>
2018-12-23 19:51:51 +0800

    update

--- src/com/chatnet/bootstrap/ClientBootstrap.java
+++ src/com/chatnet/bootstrap/ClientBootstrap.java
@@ -7,7 +7,7 @@ import javafx.stage.Stage;
 
 public class ClientBootstrap extends Application {
 	public static void main(String[] args) {
-		
+		System.out.println("holle");
 		Application.launch(args);
 	}
 
